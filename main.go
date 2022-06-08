648aa7e620ca1b189dc9aff3c1b17652ebcca810
Sam Wolfs <be.samwolfs@gmail.com>
2022-06-08 21:10:40 +0200

    Get pods list

--- main.go
+++ main.go
@@ -1,13 +1,19 @@
 package main
 
 import (
+	"flag"
 	"fmt"
 	"os"
+	"path/filepath"
 
+	"github.com/SamWolfs/kube-con-tui/pods"
 	"github.com/charmbracelet/bubbles/help"
 	"github.com/charmbracelet/bubbles/key"
 	tea "github.com/charmbracelet/bubbletea"
-	"github.com/SamWolfs/kube-con-tui/pods"
+
+	"k8s.io/client-go/kubernetes"
+	"k8s.io/client-go/tools/clientcmd"
+	"k8s.io/client-go/util/homedir"
 )
 
 type keyMap struct {
@@ -42,15 +48,33 @@ var keys = keyMap{
 	),
 }
 
-type model struct{
-	keys keyMap
-	help help.Model
+type model struct {
+	help      help.Model
+	K8sclient *kubernetes.Clientset
+	keys      keyMap
 }
 
 func initModel() model {
+	var kubeconfig *string
+	if home := homedir.HomeDir(); home != "" {
+		kubeconfig = flag.String("kubeconfig", filepath.Join(home, ".kube", "config"), "(optional) absolute path to the kubeconfig file")
+	} else {
+		kubeconfig = flag.String("kubeconfig", "", "absolute path to the kubeconfig file")
+	}
+	flag.Parse()
+
+	// use the current context in kubeconfig
+	config, err := clientcmd.BuildConfigFromFlags("", *kubeconfig)
+	if err != nil {
+		panic(err.Error())
+	}
+
+	// create the clientset
+	clientset, err := kubernetes.NewForConfig(config)
 	return model{
-		keys: keys,
-		help: help.New(),
+		help:      help.New(),
+		K8sclient: clientset,
+		keys:      keys,
 	}
 }
 
@@ -67,7 +91,10 @@ func (model model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
 		case key.Matches(msg, model.keys.Help):
 			model.help.ShowAll = !model.help.ShowAll
 		case key.Matches(msg, model.keys.Pods):
-			return pods.Model{}, nil
+			newModel := pods.Model{
+				K8sclient: model.K8sclient,
+			}
+			return newModel, newModel.Init()
 		case key.Matches(msg, model.keys.Quit):
 			return model, tea.Quit
 		}
--- pods/pods.go
+++ pods/pods.go
@@ -1,21 +1,43 @@
 package pods
 
 import (
+	"context"
+	"strings"
+
+	"github.com/charmbracelet/bubbles/help"
+	"github.com/charmbracelet/bubbles/key"
 	tea "github.com/charmbracelet/bubbletea"
+
+	v1 "k8s.io/api/core/v1"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/client-go/kubernetes"
 )
 
-type Model struct {}
+type podlist *v1.PodList
 
-func initModel(model tea.Model) Model {
-	return Model{}
+type keyMap struct {
+	Help key.Binding
+	Quit key.Binding
+}
+
+type Model struct {
+	help help.Model
+	K8sclient *kubernetes.Clientset
+	keys keyMap
+	list []string
 }
 
 func (model Model) Init() tea.Cmd {
-	return nil
+	model.list = make([]string, 1)
+	return getPods(model)
 }
 
 func (model Model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
 	switch msg := msg.(type) {
+	case podlist:
+		for _, pod := range msg.Items {
+			model.list = append(model.list, pod.Name)
+		}
 	case tea.KeyMsg:
 		switch msg.String() {
 		case "ctrl+c", "q":
@@ -27,5 +49,15 @@ func (model Model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
 }
 
 func (model Model) View() string {
-	return "We're PODS now"
+	return strings.Join(model.list, ",")
+}
+
+func getPods(model Model) tea.Cmd {
+	return func() tea.Msg {
+		pods, err := model.K8sclient.CoreV1().Pods("").List(context.TODO(), metav1.ListOptions{})
+		if err != nil {
+			panic(err.Error())
+		}
+		return podlist(pods)
+	}
 }
